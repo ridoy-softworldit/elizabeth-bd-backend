38de0d3afc7de98c240d640b819444c4651d270d
Saharior Ridoy <emonhridoy2014@gmail.com>
2026-02-28 11:52:40 +0600

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,53 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
+import router from "./routes";
+const app: Application = express();
+
+//parsers
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5000",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://www.elizabethbd.com",
+      "https://elizabethbd.com",
+      "https://admin.elizabethbd.com",
+      "https://www.admin.elizabethbd.com",
+    ],
+    credentials: true,
+  })
+);
+// Prevent Vercel edge cache from caching CORS responses
+app.use((req: Request, res: Response, next) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+});
+
+//app routes
+app.use("/api/v1", router);
+
+//root route
+app.get("/", (req: Request, res: Response) => {
+  res.send("elizabeth bd backend api server boosted on....🔥🔥🚀");
+});
+
+// //global error handler
+app.use(globalErrorHandler);
+
+// //not found route
+app.use(notFound);
+
+export default app;
